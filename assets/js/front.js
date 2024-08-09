d1d1a3271eba25bf819b821fa5072bdd8606b620
Kaltrina Delija <kaltrinadelija3@gmail.com>
2024-08-09 14:49:03 +0200

    fixed bugs

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,3 +1,5 @@
+import './custom-splide.js';
+import './modules/gallery.js';
 import './test.js';
 import './test1.js';
-import './custom-splide.js';
+
--- functions.php
+++ functions.php
@@ -53,3 +53,5 @@ add_filter('acf/settings/load_json', function($paths) {
     $paths[] = get_template_directory() . '/acf-json';
     return $paths;
 });
+
+
