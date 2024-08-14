e71fa77e332003b4eb5265e8d40cf87284dcccf8
kushtrim94 <kushtrimmarke12@gmail.com>
2024-08-14 15:55:47 +0200

    fixing small changes

--- assets/js/header-mobile-menu.js
+++ assets/js/header-mobile-menu.js
@@ -6,12 +6,23 @@ document
 
     mobileMenu.classList.toggle("hidden");
 
-    // Toggle between burger and close icon
     if (mobileMenu.classList.contains("hidden")) {
-      // Change to burger icon
-      icon.setAttribute("d", "M4 6h16M4 12h16M4 18h16"); // Burger icon path
+      icon.setAttribute("d", "M4 6h16M4 12h16M4 18h16");
     } else {
-      // Change to close (X) icon
-      icon.setAttribute("d", "M6 18L18 6M6 6l12 12"); // Close icon path
+      icon.setAttribute("d", "M6 18L18 6M6 6l12 12");
     }
   });
+
+function handleResize() {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const icon = document
+    .getElementById("mobile-menu-button")
+    .querySelector("path");
+
+  if (window.innerWidth >= 768) {
+    mobileMenu.classList.add("hidden");
+    icon.setAttribute("d", "M4 6h16M4 12h16M4 18h16");
+  }
+}
+
+window.addEventListener("resize", handleResize);
