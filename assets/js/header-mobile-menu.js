7cc56f4742dff07fc73932b355c608010ea062bd
kushtrim94 <kushtrimmarke12@gmail.com>
2024-08-15 13:08:58 +0200

    kushtrim/header-fixes

--- assets/js/header-mobile-menu.js
+++ assets/js/header-mobile-menu.js
@@ -2,10 +2,14 @@ document
   .getElementById("mobile-menu-button")
   .addEventListener("click", function () {
     const mobileMenu = document.getElementById("mobile-menu");
-    const icon = this.querySelector("path");
-
     mobileMenu.classList.toggle("hidden");
 
+    document.body.classList.toggle(
+      "overflow-hidden",
+      !mobileMenu.classList.contains("hidden")
+    );
+
+    const icon = this.querySelector("path");
     if (mobileMenu.classList.contains("hidden")) {
       icon.setAttribute("d", "M4 6h16M4 12h16M4 18h16");
     } else {
@@ -13,16 +17,17 @@ document
     }
   });
 
-function handleResize() {
-  const mobileMenu = document.getElementById("mobile-menu");
-  const icon = document
-    .getElementById("mobile-menu-button")
-    .querySelector("path");
-
-  if (window.innerWidth >= 768) {
+const menuItems = document.querySelectorAll("#mobile-menu a");
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const mobileMenu = document.getElementById("mobile-menu");
     mobileMenu.classList.add("hidden");
-    icon.setAttribute("d", "M4 6h16M4 12h16M4 18h16");
-  }
-}
 
-window.addEventListener("resize", handleResize);
+    document.body.classList.remove("overflow-hidden");
+
+    const icon = document
+      .getElementById("mobile-menu-button")
+      .querySelector("path");
+    icon.setAttribute("d", "M4 6h16M4 12h16M4 18h16");
+  });
+});
