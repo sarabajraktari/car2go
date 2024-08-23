c04790b39d7f5171bf6ddb06a3e3b7d29ccb9496
Sara Bajraktari <sara.bajraktari@g2m-ext.com>
2024-08-23 09:28:35 +0200

    removed test js

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,7 +1,5 @@
 import './modules/custom-splide.js';
 import './modules/gallery.js';
-import './test.js';
-import './test1.js';
 import './modules/custom-splide.js';
 import './modules/age-verification.js';
 import './modules/scroll-to-top.js';
