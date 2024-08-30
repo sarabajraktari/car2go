8a70584db694107e5b1aedbd09f9aa61aa37ae8f
edaballanca <e.ballanca08@gmail.com>
2024-08-30 10:11:34 -0400

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,20 @@ module.exports = {
           '0%': { transform: 'translateX(100%)', opacity: '0' },
           '100%': { transform: 'translateX(0)', opacity: '1' },
         },
+        slideInFromRightMiddle: {
+          '0%': {
+            transform: 'translateX(7vw)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
+          },
+        },
       },
       animation: {
         'slide-in': 'slideIn 1s ease-out forwards',
+        slideInFromRightMiddle: 'slideInFromRightMiddle 1.4s ease-out forwards',
       },
       maxWidth: {
         '5.5xl': '67rem',
