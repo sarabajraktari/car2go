9cf08d42661957bd42aa51aefeed6318e6e3fca3
Edonisselmanii <edonisselmani12@gmail.com>
2024-09-27 14:10:58 +0200

    Fixed the conflicts

--- app/Menus/Header.php
+++ app/Menus/Header.php
@@ -7,14 +7,20 @@ use Internship\Includes\Setup;
 class Header {
 
     public static function getData() {
-        $header = get_field('header_section','option');
-        return $header;
+        $header = get_field('header_section', 'option'); // Get header data
+
+        // Return both header and login/logout data
+        return array_merge($header, [
+            'is_logged_in' => is_user_logged_in(),  // Check if user is logged in
+            'login_url'    => site_url('/log-in'),   // URL to login page
+            'logout_url'   => wp_logout_url(home_url()),  // Logout and redirect to homepage
+        ]);
     }
 
     public static function render($data){
-        Setup::view('navigation/HeaderNavigation.twig',[
+        Setup::view('navigation/HeaderNavigation.twig', [
             'data' => $data,
         ]);
     }
 
-} 
+}
