09459e89bf75e3d7d54aa14c03e97bade5c6426a
Kaltrina Delija <kaltrinadelija3@gmail.com>
2024-09-27 10:26:08 +0200

    javascript fixes

--- assets/js/modules/SeacrchBar.js
+++ assets/js/modules/SeacrchBar.js
@@ -1,3 +1,9 @@
-document.getElementById('reset-button').addEventListener('click', function() {
-    window.location.href = 'http://internship.test/cars/?search=&brand=&city=';
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const resetButton = document.getElementById('reset-button');
+    if (resetButton) {
+      resetButton.addEventListener('click', function () {
+        window.location.href = 'http://internship.test/cars/?search=&brand=&city=';
+      });
+    }
+  });
+  
